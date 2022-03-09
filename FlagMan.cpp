ea93c9dcaef65af75a3f1e813a678f8a471e169a
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-03-09 06:57:08 -0600

    Add files via upload

---
+++ FlagMan.cpp
@@ -0,0 +1,20 @@
+/*
+Flag Man
+Pawelski
+3/9/2022
+Please follow the instructions on the activity sheet for what needs
+to be completed.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "             U  \n";
+    std::cout << "    __   .-, |  \n";
+    std::cout << "   |  | ((\"))/ \n";
+    std::cout << "   |__|_ / \\/  \n";
+    std::cout << "        /___\\  \n";
+    std::cout << "        _/ |_   \n";
+    return 0;
+}
